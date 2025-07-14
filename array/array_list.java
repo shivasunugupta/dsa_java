3b628eee5bcdc7b3b43fdebd8454ab85e30850d8
ShivasunuGupta <shivasunugupta@gmail.com>
2025-07-14 17:39:15 +0530

    Add files via upload

---
+++ array/array_list.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class array_list
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list4 = new ArrayList<Integer>();
+//        ArrayList<String> list2= new ArrayList<String>();
+//        ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+        //add element
+//        list.add(0);
+//        list.add(5);
+//        list.add(7);
+//        System.out.println(list);
+//
+//        ///get element
+//        int element= list.get(1);  //index
+//        System.out.println(element);
+//
+//        //add element
+//        list.add(1,2);
+//        System.out.println(list);
+//
+//        //set element
+//        list.set(0,8);
+//        System.out.println(list);
+//
+//        //delete elements
+//        list.remove(0); // 0 is the index
+//        System.out.println(list);
+//
+//        //size of arraylist
+//        int size = list.size();
+//        System.out.println(size);
+
+
+
+        //Loops on lists
+//        for(int i=0; i<list.size(); i++)
+//        {
+//            System.out.print(list.get(i) + " ");
+//        }
+//        System.out.println();
+
+
+        //Sorting the list
+        list.add(0);
+        list.add(5);
+        list.add(77);
+        list.add(9);
+        list.add(48);
+        list.add(3);
+
+        System.out.println(list.get(2)); //index
+
+//        Collections.sort(list);  //not for array data structure
+//        System.out.println(list);
+//
+//        list.contains(9);
+//
+//        list4.add(45);
+//        list4.add(81);
+//        list4.addAll(list);
+//        System.out.println(list4);
+
+
+    }
+}
